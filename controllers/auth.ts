d54dffd151e982fa5f6670a8ce4f47446a91ea0d
azeezabass2005 <azeezabass2005@gmail.com>
2024-03-29 21:55:06 +0100

    login functionality completed

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -6,27 +6,53 @@ import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
 export const createUser = async (req: Request, res: Response) => {
-  console.log(req.body)
-  const { firstName, lastName, email, password }: UserDocumentInt = req.body
-  if(!firstName || !lastName || !email || !password) {
-    return res.status(400).json({"message": "All credentials is required to create account"})
+  try {
+    console.log(req.body)
+    const { firstName, lastName, email, password }: UserDocumentInt = req.body
+    if(!firstName || !lastName || !email || !password) {
+      return res.status(400).json({"message": "All credentials is required to create account"})
+    }
+    const userAlreadyExists = await UserModel.findOne({ email: email })
+    if(userAlreadyExists) {
+      return res.status(409).json({"message": "User with email already exists"})
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = await createUserService({ firstName: firstName, lastName: lastName, email: email, password: hashedPassword })
+    if(!newUser) {
+      return res.status(400).json({"message": "Failed to register user"})
+    }
+    const accessToken = jwt.sign({ _id: newUser?._id, name: newUser?.firstName }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '7 days' })
+    const responseData = { ...newUser, token: accessToken }
+    console.log(responseData)
+    return res.status(201).send(responseData)
+  } catch (error) {
+    console.log(error)
+    throw error
   }
-  const userAlreadyExists = await UserModel.findOne({ email: email })
-  if(userAlreadyExists) {
-    return res.status(409).json({"message": "User with email already exists"})
-  }
-  const hashedPassword = await bcrypt.hash(password, 10)
-  const newUser = await createUserService({ firstName: firstName, lastName: lastName, email: email, password: hashedPassword })
-  if(!newUser) {
-    return res.status(400).json({"message": "Failed to register user"})
-  }
-  const accessToken = jwt.sign({ _id: newUser?._id, name: newUser?.firstName }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '7 days' })
-  const responseData = { ...newUser, token: accessToken }
-  console.log(responseData)
-  return res.status(201).send(responseData)
 }
 
-export const handleLogin = (req: Request, res: Response) => {
+export const handleLogin = async (req: Request, res: Response) => {
+ try{
   console.log(req.body)
-  return res.json({"message": "This is the route to login"})
+
+  const { email, password }: { email: string, password: string } = req.body
+  if(!email || !password) {
+    return res.status(400).json({"message": "All credentials is required to login"})
+  }
+  const userAlreadyExists = await UserModel.findOne({ email: email })
+  if(!userAlreadyExists) {
+    return res.status(400).json({"message": "User with email not found"})
+  }
+  const passwordMatch = bcrypt.compareSync(password, userAlreadyExists.password)
+  if(!passwordMatch) {
+    return res.status(400).json({"message": "Incorrect password"})
+  }
+  const accessToken = jwt.sign({ _id: userAlreadyExists._id, name: userAlreadyExists.firstName }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '7 days' })
+  const responseData = { ...userAlreadyExists, token: accessToken }
+  console.log(responseData)
+  return res.status(200).send(responseData)
+ } catch (error) {
+  console.log(error)
+  throw error
+ }
 }
