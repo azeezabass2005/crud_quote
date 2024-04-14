e2672930bf2fad1e4978c919456b91a6c6d9e054
azeezabass2005 <azeezabass2005@gmail.com>
2024-04-14 17:45:23 +0100

    chore:api integrated into the login and register forms

--- app.ts
+++ app.ts
@@ -14,6 +14,7 @@ const app: Application = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+// app.use(cors({ origin: * }))
 
 app.get("/", (req: Request, res: Response) => {
   console.log(req)
